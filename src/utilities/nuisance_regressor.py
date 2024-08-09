b06703571c72d260d3f8eca14926ced3ce1ece2d
Athena Taymourtash <athena.taymourtash@meduniwien.ac.at>
2024-08-09 11:16:35 +0200

    Updated nuisance regressors

--- src/utilities/nuisance_regressor.py
+++ src/utilities/nuisance_regressor.py
@@ -5,11 +5,14 @@ from scipy.signal import detrend
 from sklearn.decomposition import PCA
 
 class NuisanceRegressor:
-    def __init__(self, bold_nii, mask_nii=None, seg_nii=None, motion_dir=None):
+    def __init__(self, bold_nii, mask_nii=None, seg_nii=None, motion_dir=None,
+                 wm_labels=None, csf_labels=None):
         self.bold_nii = bold_nii
         self.mask_nii = mask_nii
         self.seg_nii = seg_nii
         self.motion_dir = motion_dir
+        self.wm_labels = wm_labels if wm_labels is not None else []
+        self.csf_labels = csf_labels if csf_labels is not None else []
         self.regressors = {}
         self.img, self.affine, self.header = self._load_bold_data()
         self.seg = self._load_segmentation_data()
@@ -48,22 +51,28 @@ class NuisanceRegressor:
         return global_signal
     
     def extract_wm_signal(self):
-        if self.seg is None:
-            raise ValueError("Segmentation file is required for WM signal extraction.")
-        wm_mask = self.seg == 2  
-        wm_masked_img = self.img[wm_mask & self.mask]
-        wm_signal = wm_masked_img.mean(axis=0)
-        self.regressors['wm_signal'] = wm_signal
-        return wm_signal
+        if self.seg is None and self.wm_labels: 
+            wm_mask = np.isin(self.seg, self.wm_labels)
+            wm_mask = wm_mask[self.mask]
+            wm_masked_img = self.img[wm_mask]
+            wm_signal = wm_masked_img.mean(axis=0)
+            self.regressors['wm_signal'] = wm_signal
+            return wm_signal
+        else:
+            raise ValueError("Segmentation is not available or no WM labels provided.")
+        
 
     def extract_csf_signal(self):
-        if self.seg is None:
-            raise ValueError("Segmentation file is required for CSF signal extraction.")
-        csf_mask = self.seg == 124  # Assuming 3 corresponds to CSF in segmentation
-        csf_masked_img = self.img[csf_mask & self.mask]
-        csf_signal = csf_masked_img.mean(axis=0)
-        self.regressors['csf_signal'] = csf_signal
-        return csf_signal
+        if self.seg is None and self.csf_labels:
+            csf_mask = np.isin(self.seg, self.csf_labels)
+            csf_mask = csf_mask[self.mask]
+            csf_masked_img = self.img[csf_mask]
+            csf_signal = csf_masked_img.mean(axis=0)
+            self.regressors['csf_signal'] = csf_signal
+            return csf_signal
+        else:
+            raise ValueError("Segmentation is not available or no CSF labels provided.")
+        
     
     
         

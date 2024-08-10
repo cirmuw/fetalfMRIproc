eeb83c0a8ca927e2a27cfb8911ea7d597c08934c
Athena Taymourtash <athena.taymourtash@meduniwien.ac.at>
2024-08-10 21:08:01 +0200

    Updated nuisance regressors

--- src/utilities/nuisance_regressor.py
+++ src/utilities/nuisance_regressor.py
@@ -17,6 +17,8 @@ class NuisanceRegressor:
         self.img, self.affine, self.header = self._load_bold_data()
         self.seg = self._load_segmentation_data()
         self.mask = self._load_mask_data()
+        self.regressors['motion_params'] = self._extract_motion_parameters()
+        
         
         
     def _load_bold_data(self):
@@ -51,7 +53,7 @@ class NuisanceRegressor:
         return global_signal
     
     def extract_wm_signal(self):
-        if self.seg is None and self.wm_labels: 
+        if self.seg is not None and self.wm_labels: 
             wm_mask = np.isin(self.seg, self.wm_labels)
             wm_mask = wm_mask[self.mask]
             wm_masked_img = self.img[wm_mask]
@@ -63,7 +65,7 @@ class NuisanceRegressor:
         
 
     def extract_csf_signal(self):
-        if self.seg is None and self.csf_labels:
+        if self.seg is not None and self.csf_labels:
             csf_mask = np.isin(self.seg, self.csf_labels)
             csf_mask = csf_mask[self.mask]
             csf_masked_img = self.img[csf_mask]
@@ -72,6 +74,38 @@ class NuisanceRegressor:
             return csf_signal
         else:
             raise ValueError("Segmentation is not available or no CSF labels provided.")
+        
+    def _extract_motion_parameters(self):
+        if self.motion_dir is None:
+            raise ValueError("Motion directory is required for motion regression.")
+        
+        motion_files = sorted(
+            [os.path.join(self.motion_dir, f) for f in os.listdir(self.motion_dir) 
+             if f.endswith('_v2v.tfm') and not f.startswith('.')]
+            )
+        
+        #num_volumes = self.img.shape[-1]
+        #motion_params = np.zeros((num_volumes, 6))
+        motion_params = []
+        
+        for file in motion_files:
+            params = self._extract_tfm_param(os.path.join(self.motion_dir, file))
+            #motion_params[i, :] = self._extract_tfm_param(tfm_file)
+            motion_params.append(params)
+            
+        return np.array(motion_params)
+                    
+    def _extract_tfm_param(self, tfm_file):
+        with open(tfm_file, 'r') as file:
+            lines = file.readlines()
+            for line in enumerate(lines):
+                if line.startswith('Parameters:'):
+                    params = [float(x) for x in line.split()[1:7]]  # Extract the first 6 parameters
+                    return np.array(params)
+        raise ValueError(f"Could not find motion parameters in {tfm_file}")
+        
+            
+        
         
     
     
